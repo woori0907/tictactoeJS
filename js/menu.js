2663bc66a65bf730aceec81115642810b593dbc4
Woori Kang <rose5307@naver.com>
2023-06-21 21:05:22 +0900

    230621 toggle player button

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,21 @@
 const btnNewGame = document.querySelectorAll(".new_game_button");
 const newGamePage = document.querySelector(".new_game_page");
+const toggleBtns = document.querySelectorAll(".toggle_item");
+
+const handleToggleBtn = (e) => {
+  toggleBtns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  e.target.classList.add("active");
+};
+
+toggleBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    localStorage.removeItem("player");
+    localStorage.setItem("player", e.target.dataset.playername);
+    handleToggleBtn(e);
+  });
+});
 
 btnNewGame.forEach((btn) => {
   btn.addEventListener("click", (e) => {
